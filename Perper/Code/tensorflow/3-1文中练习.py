ccb324f592c882f2e799ada1b1aa3dccc1baf6e5
WenHui-Zhou <765647930@qq.com>
2021-01-26 13:04:21 +0800

    添加tensorflow入门理论部分

---
+++ Perper/Code/tensorflow/3-1文中练习.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+# Author      : perperzhou <765647930@qq.com>
+# Create Time : 2021-01-26
+# Copyright (C)2021 All rights reserved.
+import tensorflow as tf 
+
+a = tf.constant([1,2],name = 'a',dtype = tf.float32)
+print(a)
