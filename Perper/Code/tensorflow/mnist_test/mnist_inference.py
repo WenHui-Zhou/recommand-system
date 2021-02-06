05bc14959e5a230e0cc27bc2f9c50fa56126dded
WenHui-Zhou <765647930@qq.com>
2021-02-06 18:23:34 +0800

    添加了点修改

--- Perper/Code/tensorflow/mnist_test/mnist_inference.py
+++ Perper/Code/tensorflow/mnist_test/mnist_inference.py
@@ -10,6 +10,7 @@ import tensorflow as tf
 input_node = 784
 output_onde = 10
 middle_node = 500
+# 设置一些层的参数
 
 def get_weight_variable(shape,regularizer):
     weights = tf.get_variable("weights",shape,initializer = tf.truncated_normal_initializer(stddev=0.1))
@@ -20,9 +21,11 @@ def get_weight_variable(shape,regularizer):
 def inference(input_tensor,regularizer):
 
     with tf.variable_scope('layer1'):
-        weights = get_weight_variable([input_node,middle_node],regularizer)
-        biases = tf.get_variable("biases",[middle_node],initializer = tf.constant_initializer(0.0))
-        layer1 = tf.nn.relu(tf.matmul(input_tensor,weights) + biases)
+        weights = get_weight_variable([5,5,3,16],regularizer)
+        biases = tf.get_variable("biases",[16],initializer = tf.constant_initializer(0.0))
+        layer1 = tf.nn.conv2d(input_tensor,weights,strides=[1,1,1,1],paddiing = "SAME")
+        layer1 = tf.nn.add_conv(layer1,biases)
+#        layer1 = tf.nn.relu(tf.matmul(input_tensor,weights) + biases)
     with tf.variable_scope('layer2'):
         weights = get_weight_variable([middle_node,output_onde],regularizer)
         biases = tf.get_variable("biases",[output_onde],initializer = tf.constant_initializer(0.0))
